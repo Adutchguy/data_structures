ad8314bd12e46aee1dd08e66d65e3e04f2b17f5e
Adutchguy <mdm_8@hotmail.com>
2017-04-13 14:26:32 -0700

    Updated the LinkedList file.

--- linkedList.js
+++ linkedList.js
@@ -1,15 +1,19 @@
 'use strict';
 
+function Node(value, next) {
+  this.value = values;
+  this.next = next;
+};
+
+// [5] -> [6] -> [3] -> [18]
+// ['apple'] -> ['turkey'] -> ['pie'] -> ['pb&j']
+
 function LinkedList() {
   this.head = null;
 }
 
 LinkedList.prototype.push = function(value) {
-  var x = new Node(value,this.head);
-  this.head = x;
+  var newNode = new Node(value);
+  newNode.next = this.head;
+  this.head = newNode;
 };
-
-function Node(value, next) {
-  this.value = value;
-  this.next = next ? next : null;
-}
